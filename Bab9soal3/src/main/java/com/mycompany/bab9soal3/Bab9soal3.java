29cbef9bae9a4f3ae08ad6458d5d6cba8cffd099
winanurulagisna <gisnawina8@gmail.com>
2024-08-13 07:31:51 +0700

    Add files via upload

---
+++ Bab9soal3/src/main/java/com/mycompany/bab9soal3/Bab9soal3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab9soal3;
+
+/**
+ *
+ * @author winan
+ */
+public class Bab9soal3 {
+
+    public static void main(String[] args) {
+       int i = 1;
+        while (i < 6) {
+            System.out.println(i);
+            i++; // Menambah nilai i agar loop tidak berjalan selamanya
+        }
+    }
+}
