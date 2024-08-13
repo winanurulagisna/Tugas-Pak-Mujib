000cd3215c623d872e7e6413277cafdcd3eddb86
winanurulagisna <gisnawina8@gmail.com>
2024-08-13 07:22:29 +0700

    Add files via upload

---
+++ Bab3soal1/src/main/java/com/mycompany/bab3soal1/Bab3soal1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab3soal1;
+
+/**
+ *
+ * @author winan
+ */
+public class Bab3soal1 {
+
+    public static void main(String[] args) {
+        int var1 = 9;             
+        float var2 = 8.99f;       
+        char var3 = 'A';          
+        boolean var4 = false;     
+        String var5 = "Hello World"; 
+        
+        // Menampilkan nilai-nilai variabel
+        System.out.println("var1: " + var1);
+        System.out.println("var2: " + var2);
+        System.out.println("var3: " + var3);
+        System.out.println("var4: " + var4);
+        System.out.println("var5: " + var5);
+    }
+}
