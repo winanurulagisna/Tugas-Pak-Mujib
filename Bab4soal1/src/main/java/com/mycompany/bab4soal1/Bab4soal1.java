24290c4e314c893c71ffcbf606e0c7fe436558eb
winanurulagisna <gisnawina8@gmail.com>
2024-08-13 07:23:10 +0700

    Add files via upload

---
+++ Bab4soal1/src/main/java/com/mycompany/bab4soal1/Bab4soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab4soal1;
+
+/**
+ *
+ * @author winan
+ */
+public class Bab4soal1 {
+
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 3;
+        
+        System.out.println("bilangan1 + bilangan2: " + 
+        (angka1 + angka2) );
+        System.out.println("bilangan1 - bilangan2: " + 
+        (angka1 - angka2) );
+        System.out.println("bilangan1 * bilangan2: " + 
+        (angka1 * angka2) );
+        System.out.println("bilangan1 / bilangan2: " + 
+        (angka1 / angka2) );
+    }
+}
