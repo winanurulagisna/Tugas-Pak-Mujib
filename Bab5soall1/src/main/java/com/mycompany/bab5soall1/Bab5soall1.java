e0a2c567041ca9e568d39006ab4bef0335983454
winanurulagisna <gisnawina8@gmail.com>
2024-08-13 07:26:27 +0700

    Add files via upload

---
+++ Bab5soall1/src/main/java/com/mycompany/bab5soall1/Bab5soall1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab5soall1;
+
+/**
+ *
+ * @author winan
+ */
+public class Bab5soall1 {
+
+    public static void main(String[] args) {
+        //Menetapkan nilai Variable secara langsung
+        String nama = "Andika Jaya Utama";
+        String alamat = "Ampenan";
+        int usia = 20;
+        double gaji = 10000000;
+        
+        //Menampilkan output sesuai format yang diminta
+        System.out.println("### Pendataan Karyawan PT. Petani Kode ###");
+        System.out.println("Nama Karyawan: " + nama);
+        System.out.println("Alamat       : " + alamat);
+        System.out.println("Usia         : " + usia + "tahun");
+        System.out.println("Gaji         : Rp "+ gaji);
+    }
+}
