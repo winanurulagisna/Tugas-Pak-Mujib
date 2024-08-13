2a66ffddb4792d497c2174967dd2f6e50e052df5
winanurulagisna <gisnawina8@gmail.com>
2024-08-13 07:27:22 +0700

    Add files via upload

---
+++ Bab5ssoal2/src/main/java/com/mycompany/bab5ssoal2/Bab5ssoal2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab5ssoal2;
+
+/**
+ *
+ * @author winan
+ */
+
+public class Bab5ssoal2 {
+
+    public static void main(String[] args) {
+        // Menetapkan nilai alas dan tinggi secara langsung 
+        double alas = 17;
+        double tinggi = 11;
+        
+        //Menghitung luas segitiga
+        double luas = (alas * tinggi)/2;
+        
+        // Menampilkan hasil perhitungan luas segitiga
+        System.out.println("Luas Segitiga dengan alas " + alas + " dan tinggi " + tinggi + " adalah: " + luas);
+        
+    }
+}
