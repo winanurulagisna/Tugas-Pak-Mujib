07d058062e8e4e8d1f2f9d4807e7bd424d6b3f0f
winanurulagisna <gisnawina8@gmail.com>
2024-08-13 07:21:48 +0700

    Add files via upload

---
+++ Bab2soal1/src/main/java/com/mycompany/bab2soal1/Bab2soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soal1;
+
+/**
+ *
+ * @author winan
+ */
+public class Bab2soal1 {
+
+    public static void main(String[] args) {
+        int a=50;
+        float b=a;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
