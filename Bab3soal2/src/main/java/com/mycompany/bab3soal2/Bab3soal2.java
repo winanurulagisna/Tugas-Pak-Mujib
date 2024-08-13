e368fe8678ac00f675d77b1cea0ba0300c605ea5
winanurulagisna <gisnawina8@gmail.com>
2024-08-13 07:22:48 +0700

    Add files via upload

---
+++ Bab3soal2/src/main/java/com/mycompany/bab3soal2/Bab3soal2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab3soal2;
+
+/**
+ *
+ * @author winan
+ */
+public class Bab3soal2 {
+
+    public static void main(String[] args) {
+        int NIM = 2110117000;                    
+        String namaMahasiswa = "Andika Jaya Utama";     
+        String kelas = "01SIFE020";                
+        int usia = 20;                         
+        double tinggiBadan = 1.72;
+        
+        System.out.println("Biodata Diri Mahasiswa");
+        System.out.println("-------------------------");
+        System.out.println("NIM             : " + NIM);
+        System.out.println("Nama Mahasiswa  : " + namaMahasiswa);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Usia            : " + usia + " tahun");
+        System.out.println("Tinggi Badan    : " + tinggiBadan + " cm");
+    }
+}
